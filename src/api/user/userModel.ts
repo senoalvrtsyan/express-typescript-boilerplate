253b262d250225cb936f4dd2d4110dca04fac173
Sen <alvrtsyan.seno@gmail.com>
2024-04-07 21:25:05 +0400

    Adding prisma

--- src/api/user/userModel.ts
+++ src/api/user/userModel.ts
@@ -2,15 +2,14 @@ import { z } from 'zod';
 
 import { commonValidations } from '@/common/utils/commonValidation';
 
-export type User = z.infer<typeof UserSchema>;
-export const UserSchema = z.object({
-  id: z.number(),
-  name: z.string(),
-  email: z.string().email(),
-  age: z.number(),
-  createdAt: z.date(),
-  updatedAt: z.date(),
-});
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+  age: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
 
 // Input Validation for 'GET users/:id' endpoint
 export const GetUserSchema = z.object({
--- src/api/user/userRepository.ts
+++ src/api/user/userRepository.ts
@@ -1,16 +1,44 @@
+import { PrismaClient } from '@prisma/client';
+
 import { User } from '@/api/user/userModel';
 
-export const users: User[] = [
-  { id: 1, name: 'Alice', email: 'alice@example.com', age: 42, createdAt: new Date(), updatedAt: new Date() },
-  { id: 2, name: 'Bob', email: 'bob@example.com', age: 21, createdAt: new Date(), updatedAt: new Date() },
-];
+const prisma = new PrismaClient();
 
 export const userRepository = {
   findAllAsync: async (): Promise<User[]> => {
-    return users;
+    try {
+      return await prisma.user.findMany();
+    } catch (error) {
+      console.error('Error fetching all users:', error);
+      throw error;
+    }
   },
 
-  findByIdAsync: async (id: number): Promise<User | null> => {
-    return users.find((user) => user.id === id) || null;
+  findByIdAsync: async (userId: number): Promise<User | null> => {
+    try {
+      return await prisma.user.findUnique({
+        where: {
+          id: userId,
+        },
+      });
+    } catch (error) {
+      console.error('Error fetching user by ID:', error);
+      throw error;
+    }
   },
 };
+
+// export const users: User[] = [
+//   { id: 1, name: 'Alice', email: 'alice@example.com', age: 42, createdAt: new Date(), updatedAt: new Date() },
+//   { id: 2, name: 'Bob', email: 'bob@example.com', age: 21, createdAt: new Date(), updatedAt: new Date() },
+// ];
+
+// export const userRepository = {
+//   findAllAsync: async (): Promise<User[]> => {
+//     return users;
+//   },
+//
+//   findByIdAsync: async (id: number): Promise<User | null> => {
+//     return users.find((user) => user.id === id) || null;
+//   },
+// };
